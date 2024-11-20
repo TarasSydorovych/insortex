3bb27f844ac576e943724351519c4f910bdd9240
Get Apple <getapple@Gets-MacBook-Pro.local>
2024-11-20 23:51:08 +0200

    fsd

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -1,25 +1,25 @@
-import mongoose from "mongoose";
-
-export function mongooseConnect() {
-  if (mongoose.connection.readyState === 1) {
-    return mongoose.connection.asPromise();
-  } else {
-    const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/insortex"; // Підключення до бази insortex
-    return mongoose.connect(uri);
-  }
-}
 // import mongoose from "mongoose";
 
 // export function mongooseConnect() {
 //   if (mongoose.connection.readyState === 1) {
 //     return mongoose.connection.asPromise();
 //   } else {
-//     // Формування URI з логіна, пароля та хоста з .env.local
-//     const uri = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
-
-//     return mongoose.connect(uri, {
-//       useNewUrlParser: true, // Рекомендується для уникнення помилок парсера
-//       useUnifiedTopology: true, // Використання нового двигуна для підключення
-//     });
+//     const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/insortex"; // Підключення до бази insortex
+//     return mongoose.connect(uri);
 //   }
 // }
+import mongoose from "mongoose";
+
+export function mongooseConnect() {
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection.asPromise();
+  } else {
+    // Формування URI з логіна, пароля та хоста з .env.local
+    const uri = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
+
+    return mongoose.connect(uri, {
+      useNewUrlParser: true, // Рекомендується для уникнення помилок парсера
+      useUnifiedTopology: true, // Використання нового двигуна для підключення
+    });
+  }
+}
